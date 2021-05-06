9d8ca2deae1d9f82ca082df9250602182188abee
kitty88825 <kitty40632@gmail.com>
2021-05-06 10:24:53 +0800

    News add webhook field

--- news/models.py
+++ news/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 
+from webhooks.models import Webhook
+
 
 class Reporter(models.Model):
     first_name = models.CharField(max_length=30)
@@ -14,6 +16,7 @@ class Article(models.Model):
     headline = models.CharField(max_length=100)
     pub_date = models.DateField()
     reporter = models.ForeignKey(Reporter, on_delete=models.CASCADE)
+    webhook = models.ForeignKey(Webhook, on_delete=models.SET_NULL, null=True)
 
     def __str__(self):
         return self.headline
